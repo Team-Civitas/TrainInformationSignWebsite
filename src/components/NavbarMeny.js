8b741466f2e38f2d73183243b3e56121af88ceb6
SparklesReal <jultomtenriktig@gmail.com>
2025-07-15 23:25:28 +0200

    stuff

--- src/components/NavbarMeny.js
+++ src/components/NavbarMeny.js
@@ -7,16 +7,16 @@ function NavBarMeny({ theme, setTheme }) {
     };
 
     return (
-        <div>
-            <div style={{ minWidth: '120px', width: '100vw', marginLeft: '16px', marginRight: '16px'}}>
-                <label htmlFor="ThemeSelector" style={{ display: 'block', marginBottom: '0.5rem' }}>
+        <div class="ThemeSelector">
+            <div style={{ minWidth: '120px' }}>
+                <label htmlFor="ThemeSelector" style={{ display: 'flex', marginBottom: '0.5rem' }}>
                     Theme
                 </label>
                 <select
                     id="ThemeSelector"
                     value={theme}
                     onChange={changeTheme}
-                    style={{ width: '100vw', padding: '0.5rem' }}
+                    style={{ padding: '0.5rem' }}
                 >
                     <option value="StandardTheme">StandardTheme</option>
                     <option value="2">2</option>
