c7bbedd6b7255563cc96463d6f775c6cda9d7753
DanteMinecraft <dantewallen07@gmail.com>
2025-07-16 02:57:05 +0200

    fixat tema 1

--- src/themes/StandardTheme/StandardTheme.jsx
+++ src/themes/StandardTheme/StandardTheme.jsx
@@ -9,59 +9,60 @@ function StandardTheme() {
                     <p className="currentTime">08:35</p>
                 </div>
                 <div className="grid titles">
-                    <p>Tid</p>
-                    <p>Från</p>
-                    <p>Ny tid</p>
-                    <p>Spår</p>
-                    <p>Anmärkning</p>
+                    <p className='tid'>Tid</p>
+                    <p className='från'>Från</p>
+                    <p className="nyTid" id='whiteColor'>Ny tid</p>
+                    <p className='spår'>Spår</p>
+                    <p className='anmärkning'>Anmärkning</p>
                     <div className="tågnr">Tågnummer</div>
                 </div>
             </div>
 
             <div className="grid">
-                <div>08:02</div>
-                <div>Stockholm C</div>
+                <div className='tid'>08:02</div>
+                <div className='från'>Stockholm C</div>
                 <div className="nyTid">08:40</div>
-                <div>2b</div>
-                <div>Försenad</div>
+                <div className='spår'>2b</div>
+                <div className='anmärkning'>Försenad</div>
                 <div className="tågnr">1234</div>
             </div>
 
-            <div class="grid">
-                <div>08:10</div>
-                <div>Bålsta Enköping</div>
+            <div className="grid">
+                <div className="tid">08:10</div>
+                <div className="från">Bålsta Enköping</div>
                 <div className="nyTid">09:00</div>
-                <div>1a</div>
-                <div>Sen tågvänd</div>
+                <div className="spår">1a</div>
+                <div className="anmärkning">Sen tågvänd</div>
                 <div className="tågnr">163</div>
             </div>
-            
-            <div class="grid">
-                <div>08:36</div>
-                <div>Uppsala C Stockholm C</div>
-                <div></div>
-                <div>5a</div>
-                <div>SJ Regional</div>
+
+            <div className="grid">
+                <div className="tid">08:36</div>
+                <div className="från">Uppsala C Stockholm C</div>
+                <div className="nyTid"></div>
+                <div className="spår">5a</div>
+                <div className="anmärkning">SJ Regional</div>
                 <div className="tågnr">1553</div>
             </div>
-            
-            <div class="grid">
-                <div>09:20</div>
-                <div>Bålsta Enköping</div>
-                <div></div>
-                <div>3</div>
-                <div>SJ Regional</div>
+
+            <div className="grid">
+                <div className="tid">09:20</div>
+                <div className="från">Bålsta Enköping</div>
+                <div className="nyTid"></div>
+                <div className="spår">3</div>
+                <div className="anmärkning">SJ Regional</div>
                 <div className="tågnr">643</div>
             </div>
-            
-            <div class="grid">
-                <div>09:54</div>
-                <div>Bålsta Enköping</div>
-                <div></div>
-                <div>4b</div>
-                <div>SJ Regional</div>
+
+            <div className="grid">
+                <div className="tid">09:54</div>
+                <div className="från">Bålsta Enköping</div>
+                <div className="nyTid"></div>
+                <div className="spår">4b</div>
+                <div className="anmärkning">SJ Regional</div>
                 <div className="tågnr">222</div>
             </div>
+
 
         </div>
     )
