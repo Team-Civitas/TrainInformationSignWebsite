dded06ef651b907e7ac68c9c04b2e311219fbd4e
Revilo0509 <olivers_gamecenter@outlook.com>
2025-07-17 20:55:04 +0200

    navbar structure

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,44 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../../AppContext';
 import AcquireTheme from '../../components/AcquireTheme.jsx';
 import Navbar from '../../components/Navbar/Navbar.jsx';
 
 function App() {
   const { theme, trainArray } = useContext(AppContext);
+
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [hideTimeout, setHideTimeout] = useState(null);
+
+  useEffect(() => {
+    const handleMouseMove = () => {
+      setMenuVisible(true);
+
+      if (hideTimeout) {
+        clearTimeout(hideTimeout);
+      }
+
+      const timeout = setTimeout(() => {
+        setMenuVisible(false);
+      }, 1000);
+
+      setHideTimeout(timeout);
+    };
+
+    const handleMouseLeave = () => {
+      setMenuVisible(false);
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseleave', handleMouseLeave);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseleave', handleMouseLeave);
+      if (hideTimeout) {
+        clearTimeout(hideTimeout);
+      }
+    };
+  }, [hideTimeout]);
 
   return (
     <div className="App">
