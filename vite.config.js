8019cba2848a11af36313f70f9f5cec2eda6fbfa
Revilo0509 <olivers_gamecenter@outlook.com>
2025-07-17 23:06:17 +0200

    added host:true in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     base: '/',
-    plugins: [react()]
+    plugins: [react()],
+    server: {
+        host: true,
+        port: 5173
+    },
+
 });
