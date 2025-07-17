ed09dbfa3dabcefe8d154278c7b2877c2ad104ef
Revilo0509 <olivers_gamecenter@outlook.com>
2025-07-17 21:26:28 +0200

    fixed simple theme

--- src/themes/Simple/Simple.jsx
+++ src/themes/Simple/Simple.jsx
@@ -3,36 +3,32 @@ import { stationSignatureToName } from '../../components/APIFunctions';
 import { formatTime } from '../../components/time';
 
 function Simple({ trainArray }) {
-    const now = new Date();
+  const nextTrain = trainArray.find(train => train.ActivityType !== 'Ankomst');
 
-    const nextTrain = trainArray
-        .slice()
-        .filter(t => new Date(t.AdvertisedTimeAtLocation) > now)
-        .sort((a, b) => new Date(a.AdvertisedTimeAtLocation) - new Date(b.AdvertisedTimeAtLocation))[0];
+  if (!nextTrain) {
+    return <div className="SimpleTheme">Inga kommande avgångar</div>;
+  }
 
-    if (!nextTrain) return <div className="SimpleTheme">Inga kommande avgångar</div>;
+  const toLocationArray = nextTrain.ToLocation || [];
+  const toLocationString = toLocationArray
+    .map(loc => stationSignatureToName(loc))
+    .join(' → ');
 
-    const toLocationString = nextTrain.ToLocation
-        .map(stationSignatureToName)
-        .join(' → ');
-
-    return (
-        <div className='SimpleTheme'>
-            <div>
-                <div className='SimpleThemeHeader'>
-                    <div className="DepartureTime">
-                        {formatTime(nextTrain.AdvertisedTimeAtLocation)}
-                    </div>
-                    <div>
-                        {stationSignatureToName(nextTrain.ToLocation[0])}
-                    </div>
-                </div>
-                <div>
-                    {toLocationString}
-                </div>
-            </div>
+  return (
+    <div className="SimpleTheme">
+      <div>
+        <div className="SimpleThemeHeader">
+          <div className="DepartureTime">
+            {formatTime(nextTrain.AdvertisedTimeAtLocation)}
+          </div>
+          <div>
+            {toLocationArray[0] ? stationSignatureToName(toLocationArray[0]) : 'Okänd destination'}
+          </div>
         </div>
-    );
+        <div>{toLocationString}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Simple;
